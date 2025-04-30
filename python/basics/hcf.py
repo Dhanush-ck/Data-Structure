5146213a4d771b71987facb940fd0cedf6e7fd54
Dhanush CK <dhanushck01@gmail.com>
2025-04-30 19:55:05 +0530

    docs : Added documentation

--- python/basics/hcf.py
+++ python/basics/hcf.py
@@ -1,3 +1,9 @@
+# Function to return HCF (Highest Common Factor)
+# Takes two parameters
+# First find the smallest one from the parameters
+# Then checks from 1 to the smallest number that is it divisble by both
+# If divisible it is stored into a varible
+# Variable is returned after the loop
 def calculate_hcf(x, y) :
     if(x < y):
         smallest = x
@@ -8,6 +14,9 @@ def calculate_hcf(x, y) :
             hcf = i
     return hcf
 
+# Reading first number from the user
 x = int(input("Enter the first number : "))
+# Reading second number from the user
 y = int(input("Enter the second number : "))
+# Prints HCF by calling the function
 print(f"HCF = {calculate_hcf(x, y)}")
