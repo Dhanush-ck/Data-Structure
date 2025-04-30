ac47756fcbe084047c656bde62266b00a2ce2809
Dhanush CK <dhanushck01@gmail.com>
2025-04-30 19:54:36 +0530

    docs : Added documentation

--- python/basics/grade_calculator.py
+++ python/basics/grade_calculator.py
@@ -1,11 +1,18 @@
+# Reading first mark from the user
 mark1 = int(input("Enter mark 1 : "))
+# Reading second mark from the user
 mark2 = int(input("Enter mark 2 : "))
+# Reading third mark from the user
 mark3 = int(input("Enter mark 3 : "))
+# Reading fourth mark from the user
 mark4 = int(input("Enter mark 4 : "))
+# Reading fifth mark from the user
 mark5 = int(input("Enter mark 5 : "))
 
+# Finding average mark
 avg = (mark1 + mark2 + mark3 + mark4 + mark5)/5
 
+# Prints grade according to the mark
 if(avg >= 90):
     print("Your grade is A+")
 elif(avg >= 80):
