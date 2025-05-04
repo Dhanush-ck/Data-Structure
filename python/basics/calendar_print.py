890a831f03ff6bd296b83375233ac94f6cf85d2e
Dhanush CK <dhanushck01@gmail.com>
2025-05-04 22:48:44 +0530

    feat : Added program to print calendar

---
+++ python/basics/calendar_print.py
@@ -0,0 +1,9 @@
+# Importing calendar module
+import calendar
+# Reading year
+year = int(input("Enter the year : "))
+# Reading month
+month = int(input("Enter the month : "))
+# Printing calendar
+print()
+print(calendar.month(year, month))
